6e648c57af4ba467743761e9a40f7d1e298cb43b
Antoine Ganne <antoine.ganne@hotmail.fr>
2018-05-15 15:05:06 +0200

    Ca marche!!

--- src/Client.java
+++ src/Client.java
@@ -8,8 +8,8 @@ public class Client extends Util {
     Scanner scannerConsole;
     Com communication;
     Client(){
-        super();
         scannerConsole=new Scanner(System.in);
+        communication=new Com();
     }
 
     public static void main(String[] args) {
@@ -44,51 +44,10 @@ public class Client extends Util {
     }
 
     public void connexion(String ipServer){
-        //interrogation du serveur
         System.out.println("entrez le nom de la conversation");
         String nomSalle=scannerConsole.nextLine();
-        String requete=RQ_CONNEXION_DEMANDE_TO_SERVER+" "+nomSalle;
-        envoyer(requete,ipServer,PORT_SERVER);
-
-        //reponse du serveur
-        byte[] data=new byte[100];
-        DatagramPacket dp=ecouter(data);
-        String reponse = new String(dp.getData(), StandardCharsets.UTF_8);
-        reponse=reponse.substring(0,dp.getLength());
-        Scanner scanner=new Scanner(reponse);
-        int codeRequete=Integer.valueOf(scanner.next());
-        if(codeRequete==RQ_CONNEXION_ACCEPTE_SERVER){
-            if(scanner.next().equals("hote")){
-                //cas ou la salle est déjà crée et il faut la rejoindre
-                initialisationCom(nomSalle);
-                String ipHote=scanner.next();
-                int portHote=scanner.nextInt();
-                connexionVersHoteClient(nomSalle,ipHote,portHote);
-            }else{
-                //cas ou on est l'hote
-                initialisationCom(nomSalle);
-            }
-        }
+        communication.connexion(ipServer,nomSalle);
     }
 
-    private void connexionVersHoteClient(String nomSalle, String ipHoteString, int portHote){
-        String requete=String.valueOf(RQ_CONNEXION_DEMANDE_TO_HOST);
-        InetAddress ipHote = null;
-        try {
-            ipHoteString=ipHoteString.substring(1);
-            ipHote = InetAddress.getByName(ipHoteString);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        AdresseComplete adresse=new AdresseComplete(ipHote,portHote);
-        communication.addAdresse(adresse);
 
-        envoyer(requete,ipHote,portHote);
-    }
-
-    private void initialisationCom(String nomSalle){
-        int portEcoute=getPortEcoute();
-        communication=new Com(ds,nomSalle);
-
-    }
 }
--- src/Com.java
+++ src/Com.java
@@ -1,6 +1,7 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -13,6 +14,11 @@ public class Com extends Util implements Runnable{
     }
 
     private String nomSalle;
+
+    public Com(){
+        super();
+        adresses=new ArrayList<>();
+    }
 
     public Com(DatagramSocket dsInput, String nomSalleInput){
         ds=dsInput;
@@ -34,10 +40,10 @@ public class Com extends Util implements Runnable{
             int numRequete=scan.nextInt();
             switch (numRequete){
                 case RQ_CONNEXION_DEMANDE_TO_HOST:
-//                    InetAddress ipRecue=dp.getAddress();
-//                    int portRecue= dp.getPort();
-//                    System.out.println("Accepter connexion a "+nomSalle+" au client "
-//                            +"{ ip="+ipRecue+" ,port="+portRecue);
+                    InetAddress ipRecue=dp.getAddress();
+                    int portRecue= dp.getPort();
+                    System.out.println("Accepter connexion a "+nomSalle+" au client "
+                            +"{ ip="+ipRecue+" ,port="+portRecue);
 
                 case RQ_ADD_ADRESS:
                     addAdresse(dp);
@@ -69,6 +75,51 @@ public class Com extends Util implements Runnable{
     public void addAdresse(AdresseComplete adresse){
         adresses.add(adresse);
     }
+
+
+    public void connexion(String ipServer,String nomSalle){
+        this.nomSalle=nomSalle;
+        //interrogation du serveur
+        String requete=RQ_CONNEXION_DEMANDE_TO_SERVER+" "+nomSalle;
+        envoyer(requete,ipServer,PORT_SERVER);
+
+        //reponse du serveur
+        byte[] data=new byte[100];
+        DatagramPacket dp=ecouter(data,true);  //ecoute
+        String reponse = new String(dp.getData(), StandardCharsets.UTF_8);
+        reponse=reponse.substring(0,dp.getLength());
+        Scanner scanner=new Scanner(reponse);
+        int codeRequete=Integer.valueOf(scanner.next());
+
+        if(codeRequete==RQ_CONNEXION_ACCEPTE_SERVER){
+            if(scanner.next().equals("hote")){
+                //cas ou la salle est déjà crée et il faut la rejoindre
+                String ipHote=scanner.next();
+                int portHote=scanner.nextInt();
+                connexionVersHoteClient(nomSalle,ipHote,portHote);
+            }else{
+                //cas ou on est l'hote
+            }
+        }
+        new Thread(this).start();
+    }
+
+    private void connexionVersHoteClient(String nomSalle, String ipHoteString, int portHote){
+        String requete=String.valueOf(RQ_CONNEXION_DEMANDE_TO_HOST)+" demande de connexion";
+        InetAddress ipHote = null;
+        try {
+            ipHoteString=ipHoteString.substring(1);
+            ipHote = InetAddress.getByName(ipHoteString);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        AdresseComplete adresse=new AdresseComplete(ipHote,portHote);
+        addAdresse(adresse);
+
+        envoyer(requete,ipHote,portHote);
+    }
+
+
 
 
 
